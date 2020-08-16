44ed27205230264992aea96e5586488e1a9a1071
Jose Ignacio Dominguez <jidtuit@gmail.com>
2020-08-17 00:21:25 +0200

    project setup

---
+++ src/main/java/org/jid/tests/springtestcontainers/SpringAndTestContainersPocApplication.java
@@ -0,0 +1,13 @@
+package org.jid.tests.springtestcontainers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndTestContainersPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndTestContainersPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jid/tests/springtestcontainers/SpringAndTestContainersPocApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jid.tests.springtestcontainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAndTestContainersPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
